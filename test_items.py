e1444d8d936e616361609d75a97f41f46c33e210
DiRastikhina <dianalisheva@gmail.com>
2022-07-18 23:12:00 +0300

    time 30 updated

--- test_items.py
+++ test_items.py
@@ -1,15 +1,20 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import time
-import pytest
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException  
 
-def test_find_cart_button(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+def button_exists(self):
+    try:
+        self.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    except NoSuchElementException:
+             return False
+    return True
+    
+
+def test_check_add_basket_button(browser):
+    link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-
-    button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
-
-    assert button.is_displayed(), "Кнопка добавления товара в корзину отсутсвует"
-    print ("Хорошего дня, коллега!")
-
-
+    time.sleep(30)
+    
+    assert button_exists(browser), "Seems i can't find the button to add in basket"
+    
+    
